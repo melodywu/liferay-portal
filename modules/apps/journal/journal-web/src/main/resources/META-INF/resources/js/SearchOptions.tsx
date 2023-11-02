36fa6abf8bd5cf316d5621b7420508ea0db472a5
melody <melody.wu@liferay.com>
2023-11-02 09:50:35 +0800

    Revert "LPS-200425 Use custom trigger"
    This reverts commit 698c5fb426cfd32a7c30a25925e6c3354bead11c.

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/SearchOptions.tsx
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/SearchOptions.tsx
@@ -3,34 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayButton from '@clayui/button';
 import {Option, Picker} from '@clayui/core';
 import DropDown from '@clayui/drop-down';
 import ClayForm from '@clayui/form';
-import ClayIcon from '@clayui/icon';
 import ClayLayout from '@clayui/layout';
 import {addParams, navigate} from 'frontend-js-web';
 import React, {Key} from 'react';
-
-const Trigger = React.forwardRef<HTMLButtonElement, any>(
-	(
-		{children, className: _className, onClick, triggerIcon, ...otherProps},
-		ref
-	) => (
-		<ClayButton
-			className="form-control-select"
-			displayType="secondary"
-			onClick={onClick}
-			ref={ref}
-			size="sm"
-			{...otherProps}
-		>
-			{triggerIcon && <ClayIcon className="mr-2" symbol={triggerIcon} />}
-
-			{children}
-		</ClayButton>
-	)
-);
 
 type Option = {
 	label: string;
@@ -80,11 +58,10 @@ const SearchOptions = ({
 	};
 
 	return (
-		<ClayLayout.Row className="cadmin">
+		<ClayLayout.Row>
 			<ClayLayout.Col>
 				<ClayForm.Group className="c-mr-2 d-inline-flex">
 					<Picker
-						as={Trigger}
 						id={`${namespace}searchResults`}
 						onSelectionChange={(key: Key) =>
 							onChange({results: key})
@@ -105,13 +82,11 @@ const SearchOptions = ({
 				{searchLocationOptions ? (
 					<ClayForm.Group className="c-mr-2 d-inline-flex">
 						<Picker
-							as={Trigger}
 							id={`${namespace}searchLocation`}
 							onSelectionChange={(key: Key) =>
 								onChange({location: key})
 							}
 							selectedKey={initialLocation}
-							triggerIcon="folder"
 						>
 							<DropDown.Group
 								header={Liferay.Language.get('location')}
@@ -129,7 +104,6 @@ const SearchOptions = ({
 
 				<ClayForm.Group className="d-inline-flex">
 					<Picker
-						as={Trigger}
 						id={`${namespace}searchIn`}
 						onSelectionChange={(key: Key) =>
 							onChange({searchIn: key})
